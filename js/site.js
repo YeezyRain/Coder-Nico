6121454e3396c22cf76286508ff473150662ac5d
Nicolas <reynosonicolas1111@gmail.com>
2022-12-04 13:04:48 -0300

    a
    cambio

---
+++ js/site.js
@@ -0,0 +1,33 @@
+console. log ("hola coders")
+
+/*
+let knumero = 5;
+console. log ("10")
+
+let nombre;
+nombre= "pablo";
+console. log (nombre);
+
+const anioNacimiento = 1975;
+console. log (anioNacimiento)
+
+console. log (anioNacimiento + 1)
+
+let nombreIngresado = prompt ("ingresar tu nombre");
+console. log ("hola" + nombreIngresado);
+
+const valorDolar = 41.30; 
+let montoConversion = prompt ("Ingrese el monto a comprar dolares");
+let totalDolares = Number(montoIngresado) / valorDolar; 
+alert ("Usted ha comprado " + totalDolares + "dolares americanos");
+*/
+console. log ("control-de-flujo")
+
+
+let nombre = "pablo" ;
+let edad = "30";
+let vivo = true; 
+
+if (nombre == "pablo") {
+    console. log("hola pablo") ;
+}
